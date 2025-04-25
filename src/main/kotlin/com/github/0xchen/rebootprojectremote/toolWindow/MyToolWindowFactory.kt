e40251f23d02fae47073056967532dbc5c9ba961
GitHub Action <action@github.com>
2025-04-25 07:53:12 +0000

    Template cleanup

--- src/main/kotlin/com/github/0xchen/rebootprojectremote/toolWindow/MyToolWindowFactory.kt
+++ src/main/kotlin/com/github/0xchen/rebootprojectremote/toolWindow/MyToolWindowFactory.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.toolWindow
+package com.github.0xchen.rebootprojectremote.toolWindow
 
 import com.intellij.openapi.components.service
 import com.intellij.openapi.diagnostic.thisLogger
@@ -8,8 +8,8 @@ import com.intellij.openapi.wm.ToolWindowFactory
 import com.intellij.ui.components.JBLabel
 import com.intellij.ui.components.JBPanel
 import com.intellij.ui.content.ContentFactory
-import org.jetbrains.plugins.template.MyBundle
-import org.jetbrains.plugins.template.services.MyProjectService
+import com.github.0xchen.rebootprojectremote.MyBundle
+import com.github.0xchen.rebootprojectremote.services.MyProjectService
 import javax.swing.JButton
 
 
